88fdd24613102e0dc93967aa9a0709c20a8333b8
Ali ALİN <mehmettalnn@gmail.com>
2024-01-31 00:44:50 +0300

    a

--- appMy/models.py
+++ appMy/models.py
@@ -5,16 +5,17 @@ from django.utils.text import slugify
 
 
 class Category(models.Model):
-    title = models.CharField(("Başlık"), max_length=50)    
+    title = models.CharField(("Başlık"), max_length=100, default = 0)    
     def __str__(self):
         return self.title
     
 
 class Product(models.Model):
-    title =models.CharField(("Başlık"), max_length=50)
-    price =models.FloatField(("Fiyat"))
-    text = models.TextField(("Text"), blank =True)
+    title =models.CharField(("Başlık"), max_length=100, default = 0)
+    price =models.FloatField(("Fiyat"), default = 0)
+    text = models.TextField(("Text"), blank =True, default = 0)
     img = models.ImageField(("İmage"), upload_to="product",default=0)
+    oldprice =models.FloatField(("Eski Fiyat"),default = 0)
     
     def __str__(self):
         return self.title
