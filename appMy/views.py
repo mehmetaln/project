2b6d893f3a8fec571f8b5b0d19562fe5ce5afb82
mehmetaln <mehmettalnn@gmail.com>
2024-02-11 13:04:04 +0300

    BİZE ULAŞIN KISMI TAMAMLANDI

--- appMy/views.py
+++ appMy/views.py
@@ -1,11 +1,10 @@
-# from django.core.mail import send_mail #mail gönderebilmemzi için gerekli  
-# from project.settings import EMAIL_HOST_USER 
 from django.shortcuts import render, redirect
 from appUser.models import *
 from appMy.models import *
 from django.contrib import messages
 from django.contrib.auth.models import User
-
+from django.core.mail import send_mail # bu fonksiyonu mail göndermek için kullanıyoruz views kısmında
+from project.settings import EMAIL_HOST_USER # settingsden çektigimiz host kısmımız bunu send mail içerisinde kullanacağız
 
 
 
@@ -49,12 +48,45 @@ def blogdetailPage(request,bid):
         "randomproductlist":randomproductlist[7:11],
     }
     return render(request, "blogdetail.html", context)
+
+
+
+
+# views.py
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.core.mail import send_mail
+from project.settings import EMAIL_HOST_USER
+
+@login_required
 def contactPage(request):
-    
+    if request.method == 'POST':
+        # Formdan gelen verileri al
+        ad = request.POST.get('ad')
+        eposta = request.POST.get('eposta')
+        mesaj = request.POST.get('mesaj')
 
-    context = {
-  
+        # E-posta ayarlarını belirle
+        email_settings = {
+            'use_tls': True,
+            'host': 'smtp.gmail.com',
+            'port': 587,
+            'host_user': EMAIL_HOST_USER,
+            'host_password': 'ultzvakheylfjjiw',
+        }
 
-    }
-    
-    return render(request,"contact.html", context)
+        # E-posta gönderme işlemi
+        send_mail(
+            'Konu',
+            f'Mesaj: {mesaj}\nGönderen: {ad}\nE-posta: {eposta}',
+            'from@example.com',
+            [EMAIL_HOST_USER],
+            fail_silently=False,
+            auth_user=email_settings['host_user'],
+            auth_password=email_settings['host_password'],
+        )
+
+        # İstediğiniz başka bir işlemi gerçekleştirebilirsiniz, örneğin bir teşekkür sayfasına yönlendirme yapabilirsiniz.
+
+    context = {}
+    return render(request, "contact.html", context)
--- project/settings.py
+++ project/settings.py
@@ -136,3 +136,9 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'neoshaziran@gmail.com'
 EMAIL_HOST_PASSWORD = 'ultzvakheylfjjiw'
+
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_PASSWORD = 'rdkiozsfygwfcscp'
