35b9c26dee911ffa2a64b9e6c089f83bde83a592
mehmetaln <mehmettalnn@gmail.com>
2024-03-09 21:01:19 +0300

    a

--- project/settings.py
+++ project/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 
 import os
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
